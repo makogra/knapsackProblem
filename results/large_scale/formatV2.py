a394e8c0649c2c0fae678fcb08926910e49f995e
Maciej Pietrzak <pietrzak.maciek.pv@gmail.com>
2024-11-27 15:50:58 +0100

    tag 2.0 assignment completed

--- results/large_scale/formatV2.py
+++ results/large_scale/formatV2.py
@@ -3,7 +3,7 @@ import re
 def reformat_log(file_path):
     data = {
         'Optimal Solution': [],
-        'Selection Implementation': [],
+        'Crossover implementation': [],
         'Score_Log': []
     }
     
@@ -12,19 +12,19 @@ def reformat_log(file_path):
         
     # Regular expressions for extracting the data
     optimal_solution_pattern = re.compile(r'Optimal solution = (\d*\.?\d+)')
-    selection_pattern = re.compile(r'Selection implementation: (\w+)')
+    crossover_impl_pattern = re.compile(r'Crossover implementation: (\w+)')
     score_log_pattern = re.compile(r'whole score log: \[(.*?)\]')
     
     # Extract each configuration's details and populate the dictionary
     optimal_solution = optimal_solution_pattern.findall(content)
-    selection = selection_pattern.findall(content)
+    crossover_impl = crossover_impl_pattern.findall(content)
     score_logs = score_log_pattern.findall(content)
     
     # Convert extracted strings to appropriate types and add to data dictionary
     # data['Population_Size'] = list(map(int, populations))
     # data['Crossover_Percentage'] = list(map(float, crossovers))
     data['Optimal Solution'] = list(map(float, optimal_solution))
-    data['Selection Implementation'] = list(map(str, selection))
+    data['Crossover implementation'] = list(map(str, crossover_impl))
     
     # Convert each score log into a list of floats
     for log in score_logs:
@@ -34,7 +34,7 @@ def reformat_log(file_path):
     return data
 
 # Example usage:
-file_path = 'f10_l-d_kp_20_879'  # Replace with your file path
+file_path = 'knapPI_2_2000_1000_1'  # Replace with your file path
 formatted_data = reformat_log(file_path)
 print(formatted_data)
  
